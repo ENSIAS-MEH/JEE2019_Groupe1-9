79d0753292d11ba6a2c8d59af6bdb8eea7d7ff44
safaa <khadim.safaa@gmail.com>
2018-12-13 22:29:17 +0000

    creer servlet UserController et Model UserModel et la classe Creation pour l'ajout d 'un nouveau utilisateur

--- src/web/UserModel.java
+++ src/web/UserModel.java
@@ -1,5 +1,63 @@
 package web;
 
 public class UserModel {
+	public String id;
+	public String nom;
+	public String prenom;
+	private String cin;
+	public String mail;
+	public String sexe;
+	public String nationalite;
+	private String mdp;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getCin() {
+		return cin;
+	}
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public String getNationalite() {
+		return nationalite;
+	}
+	public void setNationalite(String nationalite) {
+		this.nationalite = nationalite;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	} 
 
 }
