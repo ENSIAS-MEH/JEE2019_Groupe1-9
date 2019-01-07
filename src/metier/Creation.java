63ff7b4c5c3acf020c47546b6b6ea487b355a2c2
safaa <khadim.safaa@gmail.com>
2019-01-07 23:39:10 +0100

    nouvelle architecture du projet : suppression de tout les files

--- src/metier/Creation.java
+++
@@ -1,9 +0,0 @@
-package metier;
-
-public interface Creation {
-	public void creer( String id, String nom, String prenom, String cin, String mail, String sexe, String nationalite, String mdp) ;
-		
-		
-	
-
-}
