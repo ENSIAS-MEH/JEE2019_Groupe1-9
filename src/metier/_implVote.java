fe3fffdf197fa7079387fdcc44f9112bc5ded298
safaa <khadim.safaa@gmail.com>
2019-01-23 16:09:32 +0100

    add method _add_vote

--- src/metier/_implVote.java
+++ src/metier/_implVote.java
@@ -1,5 +1,34 @@
 package metier;
 
-public class _implVote {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import DAO.db_interaction;
+import entities._poll;
+import entities._vote;
+
+public class _implVote implements _interfaceVote {
+
+	@Override
+	public void _add_vote(_vote v) {
+		
+			// TODO Auto-generated method stub
+			Connection connection=db_interaction._get_connection();
+			try{
+			PreparedStatement ps = connection.prepareStatement
+					("insert into vote( TYPE, CHOICEID, USERID) values (?,?,?)");
+			
+			ps.setString(1,v.get_type());
+			ps.setInt(2,v.get_choiceId());
+			ps.setInt(3,v.get_userId());
+			
+
+			ps.executeUpdate();
+			}catch(SQLException e){
+			e.printStackTrace();
+			}
+		}
+
 
 }
--- src/metier/_interfaceVote.java
+++ src/metier/_interfaceVote.java
@@ -1,5 +1,9 @@
 package metier;
 
+import entities._poll;
+import entities._vote;
+
 public interface _interfaceVote {
+	public  void _add_vote(_vote p);
 
 }
